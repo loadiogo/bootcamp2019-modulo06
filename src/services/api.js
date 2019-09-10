87abab437df0434b86359817cd2137efdb29bf6b
Diogo Silva <diogosilva@MacBook-Air-de-Diogo.local>
2019-09-09 22:17:37 -0300

    fix env variables

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'http://api.github.com',
   headers: {
-    Authorization: 'token 287ca7e6d3f3cf43ffb208006d2b5dfa58fe046c',
+    Authorization: process.env.OAUTH_TOKEN,
   },
 });
 
